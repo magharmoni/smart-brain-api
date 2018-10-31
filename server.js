cc91ce60a1fadae0c1bd47db7340399483b17188
Anton <anton@magharmoni.se>
2018-10-31 17:34:36 +0100

    fix

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex(
-  console.log(process.env.DATABASE_URL);
-  {
+const db = knex({
     client: 'pg',
     connection: {
       connectionString: 'process.env.DATABASE_URL',
