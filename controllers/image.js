6799aeeb387032343e3748f2c28c6cca8ec3ed39
Anton <anton@magharmoni.se>
2022-10-23 12:43:18 +0200

    Uppdated dependencies

--- controllers/image.js
+++ controllers/image.js
@@ -20,7 +20,7 @@ const handleImage = (req, res, db) => {
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
-        res.json(entries[0]);
+        res.json(entries[0].entries);
     })
     .catch(err => res.status(400).send('unable to get entries'))
 }
